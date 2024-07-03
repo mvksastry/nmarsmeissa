952105de9dd06422ab07763c7b06ece253123d15
mvksastry <mvksastry@gmail.com>
2024-07-03 16:28:53 +0530

    superadmin related

--- resources/views/layouts/menus/sysadmin/officeSettingsAdmin.blade.php
+++ resources/views/layouts/menus/sysadmin/officeSettingsAdmin.blade.php
@@ -1,6 +1,17 @@
 <li class="nav-header">OFFICE ROLE</li>
 
 <li class="nav-item">
+					<a href="/home" class="nav-link active">
+					  <i class="nav-icon fas fa-tachometer-alt"></i>
+					  <p>
+						Dashboard
+						<!-- i class="right fas fa-angle-left"></i -->
+					  </p>
+					</a>
+				</li>
+ 
+
+<li class="nav-item">
   <a href="#" class="nav-link">
     <i class="nav-icon fas fa-chart-pie"></i>
     <p>
@@ -24,7 +35,7 @@
     </li>	
     
     <li class="nav-item">
-    <a href="{{ route('roles.index') }}" class="nav-link">
+    <a href="#" class="nav-link">
       <i class="far fa-circle nav-icon"></i>
       <p>Roles</p>
     </a>
